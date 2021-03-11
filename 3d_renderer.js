8bdb770a9975afc31880993934170a7ae22c07cc
abhisanghimire <abhisanghimire@dr-ait.org>
2021-03-12 03:32:24 +0545

    initial_commit

---
+++ 3d_renderer.js
@@ -0,0 +1,61 @@
+//2020-March-12
+//@invictus
+
+//Imported THREE.js build module,OrbitControls for 
+import * as THREE from './build/three.module.js';
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
+
+//Camera for vision, scene for platform, renderer for visualization(display);
+var camera, scene, renderer;
+
+//Created a scene & background set as mentioned color
+scene = new THREE.Scene();
+scene.background = new THREE.Color( "#f6e8c3" );
+
+//Created a PrespectiveCamera with set position
+camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.25, 3000 );
+camera.position.set( 0, 10, 15);
+
+//Created a WebGLRenderer & size set as window
+renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+//Adding rendered element to body of HTML
+document.body.appendChild( renderer.domElement );
+
+//Created a OrbitControl and updated
+var controls=new OrbitControls(camera,renderer.domElement);
+controls.update();
+
+//Created two lights
+var light = new THREE.DirectionalLight(0xdfebff, 1);
+var anotherLight = new THREE.DirectionalLight( 0xffffff );
+
+//Lights added to the scene
+scene.add( light );
+scene.add( anotherLight );
+
+//Created a GLTFLoader
+var loader = new GLTFLoader().setPath( './' );
+
+//GLTF loaded and added to scene
+loader.load( 'mountain.gltf', function ( gltf ) {
+            gltf.scene.scale.set(1.5,1.5,1.5);
+            scene.add(gltf.scene);
+} ); 
+
+//EventListener to window resize
+window.addEventListener('resize',function(){
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    camera.aspect=window.innerWidth/window.innerHeight;
+});
+
+//model is rendered
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+
+//Call function to render model
+animate();
