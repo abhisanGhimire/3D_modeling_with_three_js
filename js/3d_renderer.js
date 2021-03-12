e9971e68f4bbfd0ba41fdfc11d8b035b064c0c91
abhisanghimire <abhisanghimire@dr-ait.org>
2021-03-12 16:21:35 +0545

    example added from three.js & slight modification in file management+code

--- js/3d_renderer.js
+++ js/3d_renderer.js
@@ -2,9 +2,9 @@
 //@invictus
 
 //Imported THREE.js build module,OrbitControls for 
-import * as THREE from './build/three.module.js';
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
-import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
+import * as THREE from '../build/three.module.js';
+import { OrbitControls } from '../jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../jsm/loaders/GLTFLoader.js';
 
 //Camera for vision, scene for platform, renderer for visualization(display);
 var camera, scene, renderer;
@@ -34,9 +34,9 @@ document.body.appendChild( renderer.domElement );
 
 //Created a OrbitControl and updated
 var controls=new OrbitControls(camera,renderer.domElement);
-controls.update();
 
-//Created two lights
+//Created three lights
+const ambientLight = new THREE.AmbientLight( 0x222222 );
 var light = new THREE.DirectionalLight(0xdfebff, 1);
 var anotherLight = new THREE.DirectionalLight( 0xffffff );
 
@@ -52,6 +52,8 @@ anotherLight.castShadow = false;
 //Lights added to the scene
 scene.add( light );
 scene.add( anotherLight );
+scene.add( ambientLight );
+
 
 //Created a GLTFLoader
 var loader = new GLTFLoader().setPath( './' );
@@ -60,7 +62,7 @@ var loader = new GLTFLoader().setPath( './' );
 loader.load( 'mountain.gltf', function ( gltf ) {
             gltf.scene.scale.set(1.5,1.5,1.5);
             scene.add(gltf.scene);
-} ); 
+        }); 
 
 //EventListener to window resize
 window.addEventListener('resize',function(){
@@ -72,6 +74,7 @@ window.addEventListener('resize',function(){
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
+    controls.update();
 }
 
 //Call function to render model
---
+++ js/example_orbitloader_three.js
@@ -0,0 +1,98 @@
+//Refer to orbit example loader in thtree js for more information on source code 
+
+import * as THREE from '../build/three.module.js';
+
+import { OrbitControls } from '../jsm/controls/OrbitControls.js';
+
+let camera, controls, scene, renderer;
+
+init();
+animate();
+
+function init() {
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xcccccc );
+    scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.set( 400, 200, 0 );
+
+    // controls
+
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.listenToKeyEvents( window ); 
+
+
+    controls.enableDamping = true; 
+
+    controls.screenSpacePanning = false;
+
+    controls.minDistance = 100;
+    controls.maxDistance = 500;
+
+    controls.maxPolarAngle = Math.PI / 2;
+
+    // World
+
+    const geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
+    const material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
+
+    for ( let i = 0; i < 500; i ++ ) {
+
+        const mesh = new THREE.Mesh( geometry, material );
+        mesh.position.x = Math.random() * 1600 - 800;
+        mesh.position.y = 0;
+        mesh.position.z = Math.random() * 1600 - 800;
+        mesh.updateMatrix();
+        mesh.matrixAutoUpdate = false;
+        scene.add( mesh );
+
+    }
+
+    // Lights
+
+    const dirLight1 = new THREE.DirectionalLight( 0xffffff );
+    dirLight1.position.set( 1, 1, 1 );
+    scene.add( dirLight1 );
+
+    const dirLight2 = new THREE.DirectionalLight( 0x002288 );
+    dirLight2.position.set( - 1, - 1, - 1 );
+    scene.add( dirLight2 );
+
+    const ambientLight = new THREE.AmbientLight( 0x222222 );
+    scene.add( ambientLight );
+
+    window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    controls.update(); 
+
+    render();
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
